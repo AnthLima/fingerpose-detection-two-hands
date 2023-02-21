93b4a30661cad30686268206789c543bcaeda8d9
AnthLima <tryout3699@gmail.com>
2023-02-21 02:44:24 -0300

    [feat][main]: Adjust gestures for export with array and adjust index debug logs

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const config = {
 
   const gestureStrings = {
     'thumbs_up': '👍',
-    'scissor': '✌️',
+    'scissor': '✌🏻',
     'rock': '✊️',
     'paper': '🖐'
   }
@@ -45,7 +45,8 @@ const config = {
     // add "✌🏻" and "👍" as sample gestures
     const knownGestures = [
       fp.Gestures.VictoryGesture,
-      fp.Gestures.ThumbsUpGesture
+      fp.Gestures.ThumbsUpGesture,
+      ...gestures
     ]
     const GE = new fp.GestureEstimator(knownGestures)
     // load handpose model
@@ -74,16 +75,19 @@ const config = {
         }
         const keypoints3D = hand.keypoints3D.map(keypoint => [keypoint.x, keypoint.y, keypoint.z])
 
-        const est = GE.estimate(keypoints3D, 9)
-        if (est.gestures.length > 0) {
+        const predictions = GE.estimate(keypoints3D, 9)
+        if(!predictions.gestures.length){
+            updateDebugInfo(predictions.poseData, 'left')
+        }
+        if (predictions.gestures.length > 0) {
 
           // find gesture with highest match score
-          let result = est.gestures.reduce((p, c) => {
+          let result = predictions.gestures.reduce((p, c) => {
             return (p.score > c.score) ? p : c
           })
           const chosenHand = hand.handedness.toLowerCase()
           resultLayer[chosenHand].innerText = gestureStrings[result.name]
-          updateDebugInfo(est.poseData, chosenHand)
+          updateDebugInfo(predictions.poseData, chosenHand)
         }
 
       }
